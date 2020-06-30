7385a2fd8586ec1de652dc55c49dd598241a126e
merluzzob <66435493+merluzzob@users.noreply.github.com>
2020-06-30 16:45:35 -0400

    Update curse.py

--- curse.py
+++ curse.py
@@ -14,6 +14,115 @@ db = sqlite3.connect("assignment7.db")
 cursor = db.cursor()
 allTables = ["STUDENT", "INSTRUCTOR", "ADMIN", "COURSE"]
 
+class User:
+    # user attributes
+    firstName = ""
+    lastName = ""
+    ID = 0
+
+    # constructor
+    def __init__(self, f, l, i):
+        self.firstName = f
+        self.lastName = l
+        self.ID = i
+
+    # printing method using __str__
+    def __str__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+    def __repr__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+
+# Student:
+class Student(User):
+    # student attributes
+    gradYear = 0
+    major = ""
+    email = ""
+    schedule = []
+    # constructor
+    
+    def __init__(self, f, l, i, g, m, e):
+        User.__init__(self, f, l, i)
+        self.gradYear = g
+        self.major = m
+        self.email = e
+
+    # printing method using __str__ and __repr__
+    def __str__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+    def __repr__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+    def get_ID(self):
+        return self.ID
+    
+    def set_schedule(self, x, y, z):
+        cursor.execute("""SELECT * FROM COURSE WHERE CRN = '%s'""" % (x))
+        query_result = cursor.fetchall()
+        for i in query_result:
+            self.schedule.append(i)
+        cursor.execute("""SELECT * FROM COURSE WHERE CRN = '%s'""" % (y))
+        query_result = cursor.fetchall()
+        for i in query_result:
+            self.schedule.append(i)        
+        cursor.execute("""SELECT * FROM COURSE WHERE CRN = '%s'""" % (z))
+        query_result = cursor.fetchall()
+        for i in query_result:
+            self.schedule.append(i)    
+ 
+    def get_schedule(self):
+        return self.schedule
+    
+# Instructor:
+class Instructor(User):
+    # instructor attributes
+    title = ""
+    hireYear = 0
+    dept = ""
+    email = ""
+
+    # constructor
+    def __init__(self, f, l, i, t, hy, d, e):
+        User.__init__(self, f, l, i)
+        self.title = t
+        self.hireYear = hy
+        self.dept = d
+        self.email = e
+
+    # printing method using __str__ and __repr__
+    def __str__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+    def __repr__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+
+#  Admin:
+class Admin(User):
+    # instructor attributes
+    title = ""
+    office = ""
+    email = ""
+
+    # constructor
+    def __init__(self, f, l, i, t, o, e):
+        User.__init__(self, f, l, i)
+        self.title = t
+        self.office = o
+        self.email = e
+
+    # printing method using __str__ and __repr__
+    def __str__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+    def __repr__(self):
+        return str('Name: ' + self.firstName + ', ' + self.lastName + '.    ID: ' + str(self.ID))
+
+
+
 
 
 ######################### functions
